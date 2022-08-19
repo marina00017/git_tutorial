f1514aeb1cf6384d72943efdf1088c1856c4f6df
2173020@st.hitp.ac.jp <2173020@st.hitp.ac.jp>
2022-08-19 15:49:53 +0900

    initial commit

--- main.rb
+++ main.rb
@@ -5,4 +5,5 @@ camera = Mittsu::PerspectiveCamera.new(75.0, 1.333, 0.1, 1000.0)
 renderer = Mittsu::OpenGLRenderer.new width: 800, height: 600, title: 'RubyCamp 2022'
 
 renderer.window.run do
+  # mainによるコメント
 end
